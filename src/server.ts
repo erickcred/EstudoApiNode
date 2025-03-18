3eca68594ac6847e2d2bc42eccedae755bacce17
Erick Rick <erickcredjes@gmail.com>
2025-03-18 20:07:21 -0300

    Implementando endpoints de teste

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const app = fastify();
@@ -8,19 +9,40 @@ app.get('/hello', async () => {
   return tables;
 })
 
+app.get('/transactions', async (req, res) => {
+  const transactions = await knex('transactions').select('*');
+  return transactions;
+});
+
 app.post('/transactions', async (req, res) => {
   const { title, amount } = req.body as { title: string, amount: number };
   await knex('transactions').insert({
+    id: crypto.randomUUID(),
     title,
     amount,
   });
   return res.status(201).send();
 });
 
-app.get('/transactions', async (req, res) => {
-  const transactions = await knex('transactions').select('*');
-  return transactions;
-})
+app.put('/transactions/:id', async (req, res) => {
+  const { title, amount } = req.body as { title: string, amount: number };
+  const { id } = req.params as { id: string };
+
+  console.log(id)
+
+  await knex('transactions').update({
+    title,
+    amount,
+  }).where('id', id);
+  return res.status(204).send();
+});
+
+app.delete('/transactions/:id', async (req, res) => {
+  const { id } = req.params as { id: string };
+  await knex('transactions').delete().where('id', id);
+  return res.status(204).send();
+});
+
 
 app
   .listen({
