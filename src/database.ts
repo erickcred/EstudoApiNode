01a5ab658635b87cf187561beeec368c2a8c986d
Erick Rick <erickcredjes@gmail.com>
2025-03-18 18:05:53 -0300

    Mudando pasta do banco de dados

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { knex as setupKnex } from 'knex';
 export const config = {
   client: 'sqlite',
   connection: {
-    filename: './temp/app.db',
+    filename: './db/app.db',
   },
   useNullAsDefault: true,
 };
