b59344ed5c447b4b5bbe8c25bab3d3617a010d8d
Erick Rick <erickcredjes@gmail.com>
2025-03-22 09:44:21 -0300

    Preparando aplicação para trabalar com cookies utilizando festify/cookie. Incluindo adição do cookie no methodo POST e aplicando middleware de validação do cookie nas rotas

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,11 +3,18 @@ import crypto from 'node:crypto';
 import { z } from 'zod';
 
 import { knex } from "../database";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
-  app.get('/', async (req, res) => {
-    const transactions = await knex('transactions').select();
+  app.get('/', {
+    preHandler: [ checkSessionIdExists ],
+  }, async (req, res) => {
+    const { sessionId } = req.cookies;
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select();
 
     return {
       total: transactions.length,
@@ -15,13 +22,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
     }
   });
 
-  app.get('/:id', async (req, res) => {
+  app.get('/:id', {
+    preHandler: [ checkSessionIdExists ],
+  }, async (req, res) => {
+    const { sessionId } = req.cookies;
+
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     });
     const { id } = getTransactionParamsSchema.parse(req.params);
 
-    const transaction = await knex('transactions').where('id', id).first();
+    const transaction = await knex('transactions')
+      .where({
+        id: id,
+        session_id: sessionId,
+      })
+      .first();
 
     if (transaction)
       return res.status(200).send({
@@ -29,14 +45,18 @@ export async function transactionsRoutes(app: FastifyInstance) {
       });
 
     return res.status(404).send({
-      message: 'Transaction not found'
+      message: 'Transaction not found!'
     });
-
   });
 
-  app.get('/summary', async (req, res) => {
+  app.get('/summary', {
+    preHandler: [ checkSessionIdExists ],
+  }, async (req, res) => {
+    const { sessionId } = req.cookies;
+
     const summary = await knex('transactions')
       .sum('amount', { as: 'amount'})
+      .where('session_id', sessionId)
       .first();
 
     return res.status(200).send({
@@ -53,40 +73,74 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(req.body);
 
+    let sessionId = req.cookies.sessionId;
+    if (!sessionId) {
+      sessionId = crypto.randomUUID();
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 10 // 10 dias
+      });
+    }
+
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
       type,
+      session_id: sessionId,
     });
 
     return res.status(201).send();
   });
 
-  app.put('/:id', async (req, res) => {
-    const { title, amount } = req.body as { title: string, amount: number };
-    const { id } = req.params as { id: string };
+  app.put('/:id', {
+    preHandler: [ checkSessionIdExists ],
+  }, async (req, res) => {
+    const { sessionId } = req.cookies;
 
-    await knex('transactions').update({
-      title,
-      amount,
-    }).where('id', id);
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const getTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+    const { title, amount } = getTransactionBodySchema.parse(req.body);
+    const { id } = getTransactionParamsSchema.parse(req.params);
+
+    await knex('transactions')
+      .update({
+        title,
+        amount,
+      })
+      .where('id', id)
+      .andWhere('session_id', sessionId);
     return res.status(204).send();
   });
   
-  app.delete('/:id', async (req, res) => {
+  app.delete('/:id', {
+    preHandler: [ checkSessionIdExists ],
+  }, async (req, res) => {
+    const { sessionId } = req.cookies;
+
     const { id } = req.params as { id: string };
   
     const transaction = await knex('transactions').select('*').where('id', id).first();
 
     console.log(transaction);
     if (transaction) {
-      const t = await knex('transactions').delete().where('id', id);
+      const t = await knex('transactions')
+        .delete()
+        .where({
+          id: id,
+          session_id: sessionId,
+        });
       return res.status(204).send(t);
     }
     
     return res.status(404).send({
-      message: 'Transaction not found',
+      message: 'Transaction not found!',
       transaction: transaction
     });
   });
