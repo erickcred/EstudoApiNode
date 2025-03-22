7cadcfa1c12b71e12be6cb7f7694812cc43899aa
Erick Rick <erickcredjes@gmail.com>
2025-03-22 08:44:12 -0300

    Separando as rotas da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,13 @@
 import fastify from 'fastify';
-import crypto from 'node:crypto';
 import { knex } from './database';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transactions.routes';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
-  return tables;
-})
-
-app.get('/transactions', async (req, res) => {
-  const transactions = await knex('transactions').select('*');
-  return transactions;
+app.register(transactionsRoutes, {
+  prefix: 'transactions'
 });
-
-app.post('/transactions', async (req, res) => {
-  const { title, amount } = req.body as { title: string, amount: number };
-  await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title,
-    amount,
-  });
-  return res.status(201).send();
-});
-
-app.put('/transactions/:id', async (req, res) => {
-  const { title, amount } = req.body as { title: string, amount: number };
-  const { id } = req.params as { id: string };
-
-  console.log(id)
-
-  await knex('transactions').update({
-    title,
-    amount,
-  }).where('id', id);
-  return res.status(204).send();
-});
-
-app.delete('/transactions/:id', async (req, res) => {
-  const { id } = req.params as { id: string };
-  await knex('transactions').delete().where('id', id);
-  return res.status(204).send();
-});
-
 
 app
   .listen({
